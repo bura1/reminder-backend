7c8f9c46c0bd0a50d5e147d3e9a81ec5ddca1005
bura1 <burictomislavog@gmail.com>
2021-09-07 15:04:56 +0200

    CategoryController - basic new() function

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Entity\Reminder;
 use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CategoryController extends AbstractController
@@ -16,5 +20,20 @@ class CategoryController extends AbstractController
     {
         $categories = $categoryRepository->transformAll();
         return new JsonResponse($categories);
+    }
+
+    /**
+     * @Route("/categories/new")
+     */
+    public function new(EntityManagerInterface $entityManager)
+    {
+        $category = new Category();
+        $category->setName("Kategorija 4")
+            ->setSlug("kategorija-4");
+
+        $entityManager->persist($category);
+        $entityManager->flush();
+
+        return new Response(sprintf('New Category is ' . $category->getName()));
     }
 }
