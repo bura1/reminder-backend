c654945dff35686406d1ff4e05a26c464666adab
bura1 <burictomislavog@gmail.com>
2021-09-07 14:12:56 +0200

    ReminderController - basic new() function

--- src/Controller/ReminderController.php
+++ src/Controller/ReminderController.php
@@ -2,24 +2,35 @@
 
 namespace App\Controller;
 
+use App\Entity\Reminder;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ReminderController extends ApiController
+class ReminderController extends AbstractController
 {
     /**
      * @Route("/reminders")
      */
     public function reminders()
     {
-        return $this->respond(["aaa" => "bbb"]);
+        //return $this->respond(["aaa" => "bbb"]);
     }
 
     /**
      * @Route("/reminders/new")
      */
-    public function new()
+    public function new(EntityManagerInterface $entityManager)
     {
-        return new Response("new");
+        $reminder = new Reminder();
+        $reminder->setText("Tekst remindera 1")
+            ->setSlug("tekst-remindera-1")
+            ->setCreatedOnDate(new \DateTime());
+
+        $entityManager->persist($reminder);
+        $entityManager->flush();
+
+        return new Response(sprintf('Reminder text is ' . $reminder->getText()));
     }
 }
